dbf97dbfde1d326876e73473542e5a8ec5766b85
Srinivasulu Budharapu <srinivasulu.budharapu@vmokshagroup.com>
2018-05-03 10:01:07 +0530

    added pod.specs file

---
+++ Beerboard-Beerchip-Applebees-SDK/Beerboard_Beerchip_Applebees_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Beerboard_Beerchip_Applebees_SDK.h
+//  Beerboard-Beerchip-Applebees-SDK
+//
+//  Created by Srinivasulu Budharapu on 03/05/18.
+//  Copyright © 2018 Srinivasulu Budharapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Beerboard_Beerchip_Applebees_SDK.
+FOUNDATION_EXPORT double Beerboard_Beerchip_Applebees_SDKVersionNumber;
+
+//! Project version string for Beerboard_Beerchip_Applebees_SDK.
+FOUNDATION_EXPORT const unsigned char Beerboard_Beerchip_Applebees_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beerboard_Beerchip_Applebees_SDK/PublicHeader.h>
+
+
